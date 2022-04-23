5383a9a839dfe3c2b3f13c6cb5aa00bccf2a0e75
Hani <nellingprocess@sju.ac.kr>
2022-04-23 23:51:55 +0900

    [feat] 코디네이터 프로토콜 추가

---
+++ App/App/Sources/Common/Coordinator/SceneCoordinator.swift
@@ -0,0 +1,21 @@
+//
+//  SceneCoordinator.swift
+//  App
+//
+//  Created by Hani on 2022/04/23.
+//
+
+import UIKit
+
+protocol SceneCoordinator: Coordinator {
+    var parentCoordinator: SceneCoordinator? { get set }
+    var navigationController: UINavigationController { get set }
+    
+    func remove(childCoordinator: SceneCoordinator)
+}
+
+extension SceneCoordinator {
+    internal func remove(childCoordinator: SceneCoordinator) {
+        childCoordinators.removeAll(where: { $0 === childCoordinator })
+    }
+}
