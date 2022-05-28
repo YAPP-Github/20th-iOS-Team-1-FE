50d18800cf56ca50549d2cc54aed634079863f60
Hani <nellingprocess@sju.ac.kr>
2022-05-28 13:29:17 +0900

    [feat] SignUpAgreement UI 변경

---
+++ App/App/Sources/Common/Rx/Extension/UIButton+Rx.swift
@@ -0,0 +1,17 @@
+//
+//  UIButton+Rx.swift
+//  App
+//
+//  Created by Hani on 2022/05/28.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base: UIButton {
+    public var throttleTap: ControlEvent<Void> {
+        return ControlEvent(events: tap.throttle(.milliseconds(500), latest: false, scheduler: MainScheduler.instance))
+    }
+}
---
+++ App/App/Sources/Extension/UIButton+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  UIButton+Extension.swift
+//  App
+//
+//  Created by Hani on 2022/05/26.
+//
+
+import UIKit
+
+extension UIButton {
+    private func image(with color: UIColor) -> UIImage? {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+
+        context?.setFillColor(color.cgColor)
+        context?.fill(rect)
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return image
+    }
+
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        let backgroundImage = image(with: color)
+        
+        setBackgroundImage(backgroundImage, for: state)
+    }
+}
