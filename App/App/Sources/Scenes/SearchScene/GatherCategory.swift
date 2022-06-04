8b1e9380bd291761afb27313737b95fb6c9cd495
HanjunYu <hj56775@gmail.com>
2022-06-04 22:24:04 +0900

    [feat] GatherCategory 추가

---
+++ App/App/Sources/Scenes/SearchScene/GatherCategory.swift
@@ -0,0 +1,17 @@
+//
+//  GatherCategory.swift
+//  App
+//
+//  Created by 유한준 on 2022/06/04.
+//
+
+import Foundation
+
+enum GatherCategory {
+    case walk
+    case playground
+    case petCafe
+    case petRestaurant
+    case fair
+    case etc
+}
