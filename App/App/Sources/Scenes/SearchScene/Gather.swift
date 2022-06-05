d180711ca9b7674f44cbadde60ff4988ea6ad903
HanjunYu <hj56775@gmail.com>
2022-06-06 08:08:52 +0900

    [feat] Gather mock 객체 추가

--- App/App/Sources/Scenes/SearchScene/Gather.swift
+++ App/App/Sources/Scenes/SearchScene/Gather.swift
@@ -9,7 +9,19 @@ import Foundation
 
 struct Gather: Codable {
     private let id: Int
-    private let type: GatherCategory
     private let latitude: Double
     private let longitude: Double
+    private let type: GatherCategory
+}
+
+extension Gather {
+    internal static var mock: Self {
+        // 서울역
+        return .init(
+            id: 0,
+            latitude: 37.555797,
+            longitude: 126.973125,
+            type: .walk
+        )
+    }
 }
