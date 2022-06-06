122f17fea00836fcf2ca97e8671dd563419f844e
HanjunYu <hj56775@gmail.com>
2022-06-06 18:43:48 +0900

    [feat] 위치 권한 없을 경우 서울 시청 보여주도록 설정

---
+++ App/App/Sources/Extension/CLLocationCoordinate2D+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  CLLocationCoordinate2D+Extension.swift
+//  App
+//
+//  Created by 유한준 on 2022/06/06.
+//
+
+import CoreLocation
+
+extension CLLocationCoordinate2D: Equatable {
+    public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+        return lhs.longitude == rhs.longitude && lhs.latitude == rhs.latitude
+    }
+}
