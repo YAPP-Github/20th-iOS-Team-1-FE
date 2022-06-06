102f89df79287fe5949959406b76710249e93432
Hani <nellingprocess@sju.ac.kr>
2022-06-06 15:01:48 +0900

    [feat] 네트워크 에러 및 HTTPMethod 생성

---
+++ App/App/Sources/Common/Service/Network/NetworkError/NetworkError.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkError.swift
+//  App
+//
+//  Created by Hani on 2022/06/06.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case transportError(Error)
+    case serverError(statusCode: Int)
+    case noDataError
+}
