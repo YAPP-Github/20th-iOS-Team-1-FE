f559359f4451843292081614b239948e4c3ef7b0
Hani <nellingprocess@sju.ac.kr>
2022-06-06 15:01:48 +0900

    [feat] 네트워크 에러 및 HTTPMethod 생성

---
+++ App/App/Sources/Common/Service/Network/HTTPMethod/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  App
+//
+//  Created by Hani on 2022/06/06.
+//
+
+import Foundation
+
+enum HTTPMethod {
+    internal static let get = "GET"
+    internal static let post = "POST"
+    internal static let put = "PUT"
+    internal static let delete = "DELETE"
+}
