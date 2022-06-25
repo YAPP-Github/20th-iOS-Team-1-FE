ffead221af7387850f9abd5c8678a5899536bc13
HanjunYu <hj56775@gmail.com>
2022-06-25 17:13:11 +0900

    [feat] UIViewController Reactive Wrapper 생성

--- App/App/Sources/Extension/Reactive+Extension.swift
+++ App/App/Sources/Extension/Reactive+Extension.swift
@@ -23,3 +23,68 @@ extension Reactive where Base: MKMapView {
         return ControlEvent(events: source)
     }
 }
+
+extension Reactive where Base: UIViewController {
+    public enum ViewControllerViewState: Equatable {
+        case viewWillAppear
+        case viewDidAppear
+        case viewWillDisappear
+        case viewDidDisappear
+        case viewDidLoad
+        case viewDidLayoutSubviews
+    }
+    
+    public var viewDidLoad: Observable<Void> {
+        return methodInvoked(#selector(UIViewController.viewDidLoad))
+            .map { _ in return }
+    }
+    
+    public var viewDidLayoutSubviews: Observable<Void> {
+        return methodInvoked(#selector(UIViewController.viewDidLayoutSubviews))
+            .map { _ in return }
+    }
+    
+    public var viewWillAppear: Observable<Bool> {
+        return methodInvoked(#selector(UIViewController.viewWillAppear))
+            .map { $0.first as? Bool ?? false }
+    }
+    
+    public var viewDidAppear: Observable<Bool> {
+        return methodInvoked(#selector(UIViewController.viewDidAppear))
+            .map { $0.first as? Bool ?? false }
+    }
+    
+    public var viewWillDisappear: Observable<Bool> {
+        return methodInvoked(#selector(UIViewController.viewWillDisappear))
+            .map { $0.first as? Bool ?? false }
+    }
+    
+    public var viewDidDisappear: Observable<Bool> {
+        return methodInvoked(#selector(UIViewController.viewDidDisappear))
+            .map { $0.first as? Bool ?? false }
+    }
+    
+    public var viewState: Observable<ViewControllerViewState> {
+        return Observable.of(
+            viewDidLoad.map { _ in
+                return ViewControllerViewState.viewDidLoad
+            },
+            viewDidLayoutSubviews.map { _ in
+                return ViewControllerViewState.viewDidLayoutSubviews
+            },
+            viewWillAppear.map { _ in
+                return ViewControllerViewState.viewWillAppear
+            },
+            viewDidAppear.map { _ in
+                return ViewControllerViewState.viewDidAppear
+            },
+            viewWillDisappear.map { _ in
+                return ViewControllerViewState.viewWillDisappear
+            },
+            viewDidDisappear.map { _ in
+                return ViewControllerViewState.viewDidDisappear
+            }
+        )
+        .merge()
+    }
+}
