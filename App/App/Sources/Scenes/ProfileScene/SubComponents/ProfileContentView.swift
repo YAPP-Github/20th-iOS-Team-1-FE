cb68b01ebf108d8236dbba2bdc2ec7f1033ea829
k-nh <skgml977@gmail.com>
2022-06-29 00:11:17 +0900

    [feat] 마이페이지 FooterView 추가

--- App/App/Sources/Scenes/ProfileScene/SubComponents/ProfileContentView.swift
+++ App/App/Sources/Scenes/ProfileScene/SubComponents/ProfileContentView.swift
@@ -14,13 +14,30 @@ final class ProfileContentView: UIView {
         
         return headerView
     }()
+    
+    
+    private lazy var addPuppyButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("반려견 추가", for: .normal)
+        button.setTitleColor(.Togaether.mainGreen, for: .normal)
+        button.titleLabel?.font = .customFont(size: 14, style: .Regular)
+        button.setImage(.Togaether.plus, for: .normal)
+        button.semanticContentAttribute = .forceRightToLeft
+        button.layer.cornerRadius = 15
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.Togaether.mainGreen.cgColor
+        button.tintColor = UIColor.Togaether.mainGreen
+        
+        return button
+    }()
 
     private lazy var dogListView: UITableView = {
-        let tableView = UITableView()
+        let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.separatorStyle = .none
         tableView.allowsSelection = false
+        tableView.backgroundColor = .Togaether.background
         tableView.tableHeaderView = profileHeaderView
         tableView.registerCell(type: DogTableViewCell.self)
         
@@ -59,7 +76,7 @@ final class ProfileContentView: UIView {
 
 extension ProfileContentView: UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -69,8 +86,28 @@ extension ProfileContentView: UITableViewDataSource, UITableViewDelegate{
         
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 132
+    }
+
+    func tableView(_ tableView: UITableView,
+                   viewForFooterInSection section: Int) -> UIView? {
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: dogListView.frame.width, height: 100))
+        footerView.addSubview(addPuppyButton)
+        NSLayoutConstraint.useAndActivateConstraints([
+            addPuppyButton.centerXAnchor.constraint(equalTo: footerView.centerXAnchor),
+            addPuppyButton.centerYAnchor.constraint(equalTo: footerView.centerYAnchor),
+            addPuppyButton.widthAnchor.constraint(equalToConstant: 102),
+            addPuppyButton.heightAnchor.constraint(equalToConstant: 33)
+            ])
+        footerView.backgroundColor = .Togaether.background
+
+        return footerView
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   heightForFooterInSection section: Int) -> CGFloat {
+        return 100
     }
 }
