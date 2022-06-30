d05481ebee6614ca968f030bced2fee077479956
k-nh <skgml977@gmail.com>
2022-07-01 03:44:58 +0900

    [feat] 도메인 모델 추가

---
+++ App/App/Sources/Scenes/ProfileScene/MainScene/Entity/AccountInfo.swift
@@ -0,0 +1,17 @@
+//
+//  AccountInfo.swift
+//  App
+//
+//  Created by 김나희 on 7/1/22.
+//
+
+import Foundation
+
+struct AccountInfo: Equatable {
+    var nickName: String? = nil
+    var address: String? = nil
+    var age: String? = nil
+    var Introduction: String? = nil
+    var categories: [String]?
+    var profileImageURL: String?
+}
