373e2b07a6bf3e11d09c605c3f97eb0644ef894a
HanjunYu <hj56775@gmail.com>
2022-07-02 00:44:08 +0900

    [fix] GatherRepository 요청 성공

--- App/App/Sources/Scenes/SearchScene/SearchReactor.swift
+++ App/App/Sources/Scenes/SearchScene/SearchReactor.swift
@@ -18,26 +18,34 @@ final class SearchReactor: Reactor {
     
     enum Mutation {
         case setVisibleCoordinate(CLLocationCoordinate2D)
+        case setMapViewAnnotation([GatherConfigurationForAnnotation])
+        case loadingAnnotation(Bool)
     }
     
     struct State {
         var visibleCorrdinate: Coordinate = .seoulCityHall
-        
         var currentSpan: Double = 0.005
+        var annotations: [GatherConfigurationForAnnotation] = []
+        var isAnnotationLoading: Bool = true
     }
-
-    internal let initialState: State
     
-    init() {
+    internal let initialState: State
+    private let disposeBag = DisposeBag()
+    private let gatherRepository: GatherRepositoryInterface
+    
+    init(gatherRepository: GatherRepositoryInterface) {
         self.initialState = State()
+        self.gatherRepository = gatherRepository
     }
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
-
+            
         case let .mapViewVisibleRegionDidChanged(topLeftCoordinate, rightBottomCoordinate):
-//            return Observable.just(.setVisibleCoordinate(newCoordinate))
-            return .never()
+            return searchMapViewAnnotation(
+                topLeftCoordinate: topLeftCoordinate,
+                rightBottomCoordinate: rightBottomCoordinate
+            )
         }
     }
     
@@ -45,14 +53,45 @@ final class SearchReactor: Reactor {
         var newState = state
         
         switch mutation {
-
+            
         case .setVisibleCoordinate(let newCoordinate):
             newState.visibleCorrdinate = Coordinate(
                 latitude: newCoordinate.latitude,
                 longitude: newCoordinate.longitude
             )
+            
+        case .setMapViewAnnotation(let gatherConfigurationsForAnnotation):
+            newState.annotations = gatherConfigurationsForAnnotation
+            
+        case .loadingAnnotation(let isSuccess):
+            newState.isAnnotationLoading = isSuccess
         }
         
         return newState
+    }
+    
+    private func searchMapViewAnnotation(
+        topLeftCoordinate: CLLocationCoordinate2D,
+        rightBottomCoordinate: CLLocationCoordinate2D
+    ) -> Observable<Mutation> {
+        return Observable.create { [unowned self] observer in
+            self.gatherRepository
+                .requsetGatherConfigurationForAnnotation(
+                    topLeftCoordinate: topLeftCoordinate,
+                    bottomRightCoordinate: rightBottomCoordinate
+                )
+                .subscribe { result in
+                    switch result {
+                    case .success(let gatherConfigrationsForAnnotations):
+                        observer.onNext(
+                            Mutation.setMapViewAnnotation(gatherConfigrationsForAnnotations)
+                        )
+                    case .failure(let error):
+                        print("RESULT FAILURE: ", error.localizedDescription)
+                        observer.onNext(Mutation.loadingAnnotation(false))
+                    }
+                }.disposed(by: self.disposeBag)
+            return Disposables.create()
+        }
     }
 }
