c88a40be8639743708788723fd1d2228d7a99b1f
HanjunYu <hj56775@gmail.com>
2022-07-04 22:14:44 +0900

    [feat] GahterRepository Sheet 요청 메소드 추가

--- App/App/Sources/Common/Repository/GatherRepository.swift
+++ App/App/Sources/Common/Repository/GatherRepository.swift
@@ -11,17 +11,22 @@ import Foundation
 import RxSwift
 
 protocol GatherRepositoryInterface {
-    func requsetGatherConfigurationForAnnotation(
+    func requestGatherConfigurationForAnnotation(
         topLeftCoordinate: CLLocationCoordinate2D,
         bottomRightCoordinate: CLLocationCoordinate2D
     ) -> Single<[GatherConfigurationForAnnotation]>
+    
+    func requsetGatherConfigurationForBottomSheet(
+        gatherID: Int,
+        userLocation: CLLocationCoordinate2D
+    ) -> Single<GatherConfigurationForSheet>
 }
 
-struct GatherConfigurationForAnnotationDTO: Codable {
-    internal var upperLeftLatitude: Double
-    internal var upperLeftLongitude: Double
-    internal var bottomRightLatitude: Double
-    internal var bottomRightLongitude: Double
+struct GatherConfigurationForAnnotationRequestDTO: Codable {
+    private var upperLeftLatitude: Double
+    private var upperLeftLongitude: Double
+    private var bottomRightLatitude: Double
+    private var bottomRightLongitude: Double
     internal var toQueryString: String {
         "upperLeftLatitude=\(upperLeftLatitude)&upperLeftLongitude=\(upperLeftLongitude)&bottomRightLatitude=\(bottomRightLatitude)&bottomRightLongitude=\(bottomRightLongitude)"
     }
@@ -37,6 +42,21 @@ struct GatherConfigurationForAnnotationDTO: Codable {
     }
 }
 
+struct GatherConfigurationForSheetRequestDTO: Codable {
+    private var id: Int
+    private var userLatitude: Double
+    private var userLongitude: Double
+    internal var toQueryString: String {
+        "\(id)?userLatitude=\(userLatitude)&userLongitude=\(userLongitude)"
+    }
+    
+    init(id: Int, coordinate: CLLocationCoordinate2D) {
+        self.id = id
+        self.userLatitude = coordinate.latitude
+        self.userLongitude = coordinate.longitude
+    }
+}
+
 final class GatherRepository: GatherRepositoryInterface {
     private let networkManager: NetworkManageable
     private let disposeBag = DisposeBag()
@@ -45,7 +65,7 @@ final class GatherRepository: GatherRepositoryInterface {
         self.networkManager = networkManager
     }
     
-    internal func requsetGatherConfigurationForAnnotation(
+    internal func requestGatherConfigurationForAnnotation(
         topLeftCoordinate: CLLocationCoordinate2D,
         bottomRightCoordinate: CLLocationCoordinate2D
     ) -> Single<[GatherConfigurationForAnnotation]> {
@@ -54,7 +74,7 @@ final class GatherRepository: GatherRepositoryInterface {
                 return Disposables.create()
             }
             
-            let dto = GatherConfigurationForAnnotationDTO(
+            let dto = GatherConfigurationForAnnotationRequestDTO(
                 topLeftCoordinate: topLeftCoordinate,
                 bottomRightCoordinate: bottomRightCoordinate
             )
@@ -95,5 +115,58 @@ final class GatherRepository: GatherRepositoryInterface {
             
             return Disposables.create()
         }
+    }
+    
+    internal func requsetGatherConfigurationForBottomSheet(
+        gatherID: Int,
+        userLocation: CLLocationCoordinate2D
+    ) -> Single<GatherConfigurationForSheet> {
+        return Single<GatherConfigurationForSheet>.create { [weak self] observer in
+            guard let self = self else {
+                return Disposables.create()
+            }
+            
+            let dto = GatherConfigurationForSheetRequestDTO(
+                id: gatherID,
+                coordinate: userLocation
+            )
+            
+            guard let url = URL(string: "https://yapp-togather.com/api/clubs/search/range/\(dto.toQueryString)")
+            else {
+                return Disposables.create()
+            }
+            
+            let keychain = KeychainQueryRequester()
+            let keychainProvider = KeychainProvider(keyChain: keychain)
+            guard let Token = try? keychainProvider.read(
+                service: KeychainService.apple,
+                account: KeychainAccount.accessToken
+            ) else {
+                print("토큰이 존재하지 않습니다.")
+                return Disposables.create()
+            }
+            let accessToken = "Bearer " + (String(data: Token, encoding: .utf8) ?? "")
+            
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = HTTPMethod.get
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+            urlRequest.addValue(accessToken, forHTTPHeaderField: "Authorization")
+            
+            let response: Single<GatherConfigurationForSheet> = self.networkManager.requestDataTask(with: urlRequest)
+            
+            response.subscribe { result in
+                switch result {
+                case .success(let gatherConfigurationForSheet):
+                    observer(.success(gatherConfigurationForSheet))
+                case .failure(let error):
+                    observer(.failure(error))
+                }
+            }
+            .disposed(by: self.disposeBag)
+            
+            return Disposables.create()
+        }
+
     }
 }
