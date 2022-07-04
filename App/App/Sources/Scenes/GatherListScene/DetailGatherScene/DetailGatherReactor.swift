86b3d1e2d50bde9f8846f9962c8e59d6914e84a1
Hani <nellingprocess@sju.ac.kr>
2022-07-04 23:58:42 +0900

    [feat] DetailGather 일부 구현

---
+++ App/App/Sources/Scenes/GatherListScene/DetailGatherScene/DetailGatherReactor.swift
@@ -0,0 +1,43 @@
+//
+//  DetailGatherReactor.swift
+//  App
+//
+//  Created by Hani on 2022/07/04.
+//
+
+import Foundation
+
+import ReactorKit
+import RxSwift
+
+class DetailGatherReactor: Reactor {
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        
+    }
+    
+    let initialState = State()
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+            
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        
+        switch mutation {
+            
+        }
+        
+        return newState
+    }
+}
