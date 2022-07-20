9fffdd4d7cb653b6068dd7cbdf6545ba3bcec0de
Hani <nellingprocess@sju.ac.kr>
2022-07-21 01:13:43 +0900

    [feat] API ID Update

---
+++ App/App/Sources/Scenes/GatherScene/Entitiy/ClubDetailInfo.swift
@@ -0,0 +1,26 @@
+//
+//  ClubDetailInfo.swift
+//  App
+//
+//  Created by Hani on 2022/07/21.
+//
+
+import Foundation
+
+struct ClubDetailInfo {
+    let id: Int
+    let title: String
+    let description: String
+    let category: String
+    let meetingPlace: String
+    let latitude: Double
+    let longitude: Double
+    let startDate: String
+    let endDate: String
+    let eligiblePetSizeTypes: [String]
+    let eligibleBreeds: [String]
+    let eligibleSex: String
+    let maximumPeople: Int
+    let participants: Int
+}
+
---
+++ App/App/Sources/Scenes/GatherScene/Entitiy/CommentInfo.swift
@@ -0,0 +1,16 @@
+//
+//  CommentInfo.swift
+//  App
+//
+//  Created by Hani on 2022/07/21.
+//
+
+import Foundation
+
+struct CommentInfo {
+    let id: Int
+    let content: String
+    let author: String
+    let leader: String
+    let updatedTime: String
+}
