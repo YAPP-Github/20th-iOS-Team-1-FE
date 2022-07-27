bf99c4f546eb37ffe0e06b612f99f625ea69c2e1
k-nh <skgml977@gmail.com>
2022-07-27 22:30:03 +0900

    [refactor] 탭바 아이템 이미지 등록

--- App/App/Sources/Common/Coordinator/TabCoordinator.swift
+++ App/App/Sources/Common/Coordinator/TabCoordinator.swift
@@ -17,29 +17,31 @@ final class TabCoordinator: Coordinator {
     }
     
     func start() {
-        let secondCoordinator = GatherListCoordinator()
+        let secondCoordinator = SearchCoordinator()
         childCoordinators.append(secondCoordinator)
         secondCoordinator.parentCoordinator = self
         secondCoordinator.start()
         let secondViewController = secondCoordinator.navigationController
-        secondViewController.tabBarItem = UITabBarItem(title: "Gather", image: nil, selectedImage: nil)
+        secondViewController.tabBarItem = UITabBarItem(title: "찾기", image: .Togaether.searchIcon, selectedImage: .Togaether.searchIconFill)
         
-        let thirdCoordinator = SearchCoordinator()
+        let thirdCoordinator = GatherListCoordinator()
         childCoordinators.append(thirdCoordinator)
         thirdCoordinator.parentCoordinator = self
         thirdCoordinator.start()
         let thirdViewController = thirdCoordinator.navigationController
-        thirdViewController.tabBarItem = UITabBarItem(title: "Search", image: nil, selectedImage: nil)
+        thirdViewController.tabBarItem = UITabBarItem(title: "내모임", image: .Togaether.gatherIcon, selectedImage: .Togaether.gatherIconFill)
+        
         
         let fourthCoordinator = ProfileCoordinator()
         childCoordinators.append(fourthCoordinator)
         fourthCoordinator.parentCoordinator = self
         fourthCoordinator.start()
         let fourthViewController = fourthCoordinator.navigationController
-        fourthViewController.tabBarItem = UITabBarItem(title: "Profile", image: nil, selectedImage: nil)
+        fourthViewController.tabBarItem = UITabBarItem(title: "마이페이지", image: .Togaether.myPageIcon, selectedImage: .Togaether.myPageIconFill)
         
         let tabBarController = UITabBarController()
         tabBarController.viewControllers = [secondViewController, thirdViewController, fourthViewController]
+        tabBarController.tabBar.tintColor = .Togaether.primaryLabel
         
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
