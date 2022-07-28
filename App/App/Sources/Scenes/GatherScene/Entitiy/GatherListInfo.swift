8915bec343b56e17492f09b8b568263e6fede7ef
k-nh <skgml977@gmail.com>
2022-07-28 22:15:05 +0900

    [feat] 모임 리스트 UI 업데이트 및 Entity 수정

--- App/App/Sources/Scenes/GatherScene/Entitiy/GatherListInfo.swift
+++ App/App/Sources/Scenes/GatherScene/Entitiy/GatherListInfo.swift
@@ -23,15 +23,37 @@ struct ClubInfos: Equatable {
 
 struct Content: Equatable {
     var clubID: Int
-    var title, category, meetingPlace: String?
-    var startDate, endDate: String?
+    var title, meetingPlace: String
+    var category: GatherCategory?
+    var startDate, endDate: Date?
     var eligibleBreeds: [String]?
-    var eligiblePetSizeTypes: [String]?
-    var eligibleSex: String?
-    var maximumPeople, participants: Int
-
-    enum CodingKeys: String, CodingKey {
-        case clubID = "clubId"
-        case title, category, meetingPlace, startDate, endDate, eligibleBreeds, eligiblePetSizeTypes, eligibleSex, maximumPeople, participants
+    var eligiblePetSizeTypes: [PetSizeType]
+    var eligibleSex: OwnerSex
+    var maximumPeople,participants: Int
+    
+    
+    init(clubID: Int,
+         title: String,
+         category: String,
+         meetingPlace: String,
+         startDate: String,
+         endDate: String,
+         eligibleBreeds: [String]?,
+         eligiblePetSizeTypes: [String],
+         eligibleSex: String,
+         maximumPeople: Int,
+         participants: Int
+    ) {
+        self.clubID = clubID
+        self.title = title
+        self.category = GatherCategory(rawValue: category)
+        self.meetingPlace = meetingPlace
+        self.startDate = startDate.toDate()
+        self.endDate = endDate.toDate()
+        self.eligibleBreeds = eligibleBreeds
+        self.eligiblePetSizeTypes = eligiblePetSizeTypes.map { PetSizeType(rawValue: $0) ?? .all }
+        self.eligibleSex = OwnerSex(rawValue: eligibleSex) ?? .all
+        self.maximumPeople = maximumPeople
+        self.participants = participants
     }
 }
